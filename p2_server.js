2403901f833623e12faadbfa930b00bea577c2be
linziyi <linziyi_nanjin@sina.com>
2026-09-05 17:33:39 +0800

    feat:delete

--- p2_server.js
+++ p2_server.js
@@ -2,12 +2,20 @@ import express from "express";
 import path from "node:path";
 import { dirname } from "./server.js";
 import multer from "multer";
+import { unlink } from "node:fs/promises";
 
 const router = express.Router();
 
 const upload = multer({
     dest: "uploads/"
 });
+
+async function del(del_list) {
+    for (const filename of del_list) {
+        const filePath = path.join(dirname, "uploads", filename);
+        await unlink(filePath);
+    }
+}
 
 router.get("/p2", (request, response) => {
     response.sendFile(path.join(dirname, "public", "p2.html"));
@@ -32,5 +40,16 @@ router.post("/p2", upload.single("upload"), (request, response) => {
         }
     });
 });
+
+router.delete("/p2", async (request, response) => {
+    const del_list = request.body.filenames;
+    if (!Array.isArray(del_list) || del_list.length == 0) {
+        return response.status(400).json({
+            answer: "请选择要删除的文件"
+        });
+    }
+    await del(del_list);
+    response.status(200).json({ answer: "成功删除" });
+})
 
 export { router };
--- public/p2.js
+++ public/p2.js
@@ -2,12 +2,16 @@
  * @Author: linziyi linziyi_nanjin@sina.com
  * @Date: 2026-09-05 10:42:05
  * @LastEditors: linziyi linziyi_nanjin@sina.com
- * @LastEditTime: 2026-09-05 15:53:31
+ * @LastEditTime: 2026-09-05 17:29:04
  * @FilePath: \node_4\public\p2.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+const list = document.getElementById("list");
 const form = document.getElementById("form");
 form.addEventListener("submit", handler);
+const del = document.getElementById("del");
+del.addEventListener("click", clear)
+const li = document.getElementsByTagName("li");
 
 async function handler(e) {
     e.preventDefault();
@@ -24,11 +28,52 @@ async function handler(e) {
         const { answer } = result;
         const { message } = result;
         const { originalname, destination, filename, size } = message;
+        const listItem = document.createElement("li");
+        const check = document.createElement("input");
+        const label = document.createElement("label");
+        check.id = filename;
+        check.type = "checkbox";
+        label.htmlFor = filename;
+        label.textContent = originalname;
+        listItem.name = filename;
+        listItem.append(check, label);
+        list.append(listItem);
         alert(`${answer}
             ${originalname}
             ${destination}
             ${filename}
-            ${size}`)
+            ${size}`);
+    } catch (err) {
+        alert(err);
+        console.log(err);
+    }
+}
+
+async function clear(e) {
+    e.preventDefault();
+    let del_list = new Set();
+    [...li].forEach((li) => {
+        const check = li.children[0];
+        if (check.checked) {
+            del_list.add(`${check.id}`);
+            li.remove();
+        }
+    })
+    try {
+        const response = await fetch("/home/p2", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                filenames: [...del_list]
+            })
+        });
+        const result = await response.json();
+        if (!response.ok) {
+            throw result.answer;
+        }
+        alert(`${result.answer}`);
     } catch (err) {
         alert(err);
         console.log(err);
